1ae4810479be8daf88368fcd886d4ecce860f28d
Alternosantos <alberto1fsantos@gmail.com>
2026-05-05 17:10:21 +0100

    git ignore fix + basic set up

---
+++ Light Souls/Game1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Reflection.Emit;
+
+namespace Light_Souls
+{
+    public class PlatformerGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        // We'll add our player, level, etc. here later
+        private Player _player;
+        private Level _level;
+
+        public PlatformerGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            // Set window size (optional)
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 480;
+            _graphics.ApplyChanges();
+        }
+
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Create player texture (32x32 white square)
+            Texture2D playerTex = CreateSolidTexture(32, 32, Color.Green);
+            Vector2 startPos = new Vector2(100, 100);
+            _player = new Player(playerTex, startPos);
+
+            // Create tile textures
+            Texture2D grassTex = CreateSolidTexture(32, 32, Color.Green);
+            Texture2D dirtTex = CreateSolidTexture(32, 32, Color.Brown);
+            _level = new Level(grassTex, dirtTex);
+
+            // Give the player the tile array from level
+            // (You'll need to modify Player.Update to accept tiles)
+        }
+        private Texture2D CreateSolidTexture(int width, int height, Color color)
+        {
+            Texture2D texture = new Texture2D(GraphicsDevice, width, height);
+            Color[] data = new Color[width * height];
+            for (int i = 0; i < data.Length; i++) data[i] = color;
+            texture.SetData(data);
+            return texture;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // Update game objects
+            _player.Update(gameTime, _level.GetTiles());
+            _level?.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _level?.Draw(_spriteBatch);
+            _player?.Draw(_spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
